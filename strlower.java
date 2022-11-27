33ff070ddb4c4554e7a920821fbfedbbf93bb3b3
mayank4singh <mailatmausam@gmail.com>
2022-11-27 22:07:38 +0530

    sixth commit

---
+++ strlower.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class strlower {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter  a character");
+        String a=sc.nextLine();
+        a=a.toLowerCase();
+        System.out.println(a);
+    }
+}
