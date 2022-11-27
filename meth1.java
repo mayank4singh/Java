b2849c919d1f6e60c9841bb6bb4c4ac8d461ba5b
mayank4singh <mailatmausam@gmail.com>
2022-11-27 20:06:29 +0530

    sixth commit

---
+++ meth1.java
@@ -0,0 +1,14 @@
+//This is first question of method assignment
+public class meth1 {
+int getTotal(int a,int b)
+{
+ return a+b; 
+}
+ public static void main(String[] args)
+ { 
+meth1 obj = new meth1(); 
+int c = obj.getTotal(5,4);
+ System.out.println(c);
+ } }
+
+
