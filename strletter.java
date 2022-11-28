05723997f334613bd63a0b5bc715ae22723aaa05
mayank4singh <mailatmausam@gmail.com>
2022-11-28 22:40:23 +0530

    sixth commit

---
+++ strletter.java
@@ -0,0 +1,6 @@
+public class strletter {
+    public static void main(String[] args) {
+        String letter="Dear Sir, \n\tThis Java Course is nice. \nThanks";
+        System.out.println(letter);
+    }
+}
